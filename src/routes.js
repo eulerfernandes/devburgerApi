28c5a79056b931ec85a12be91c07d05617838d2a
eulerfernandes <pedraeuler@gmail.com>
2024-12-02 21:34:58 -0300

    alterações

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,8 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
-
-import User from './app/models/user';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'leticia',
-    email: 'leti@gmail.com',
-    password_hash: 'njndjnncd1254dccd',
-  });
-
-  return response.status(201).json(user);
-});
+routes.get('/users', UserController.store);
 
 export default routes;
