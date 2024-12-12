90de838442ce50f85f6e5e4b720b5e433af17b9b
eulerfernandes <pedraeuler@gmail.com>
2024-12-12 18:54:44 -0300

     Criando Hash de senha com bcrypt

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,11 +1,12 @@
 import Sequelize, { Model } from 'sequelize';
-
+const bcrypt = require('bcrypt');
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         admin: Sequelize.BOOLEAN,
       },
@@ -13,6 +14,14 @@ class User extends Model {
         sequelize,
       },
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10);
+      }
+    });
+
+    return this;
   }
 }
 
