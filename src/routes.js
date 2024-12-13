682b2bb70aa9b134bba74334f647478a280985d3
eulerfernandes <pedraeuler@gmail.com>
2024-12-13 20:26:04 -0300

    logging in user

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionControllers from './app/controllers/SessionControllers';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionControllers.store);
 
 export default routes;
