b9b37b85fd28833a98626d5c867d13db61b5c58f
eulerfernandes <pedraeuler@gmail.com>
2024-12-16 22:36:53 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionControllers from './app/controllers/SessionControllers';
+import ProductController from './app/controllers/ProductController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionControllers.store);
+routes.post('/product', ProductController.store);
 
 export default routes;
